9b7e0139fe7e4d9ff83f544b0f15e843ce70f6ab
nikolay-waveo <nikolay@waveo.com>
2022-01-13 09:39:24 +0800

    Feat: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
-
-  return defineConfig({
-    root: './',
-    // base: `/${process.env.VITE_APP_NAME}/`,
-    plugins: [react()]
-  })
-}
+export default defineConfig({
+  root: './',
+  plugins: [react()]
+})
