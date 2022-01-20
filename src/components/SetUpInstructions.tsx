2ff5083b5935040d0bc7dded1ce139a19bd70715
nikolay-waveo <nikolay@waveo.com>
2022-01-20 16:28:14 +0800

    Feat: Update styling

--- src/components/SetUpInstructions.tsx
+++ src/components/SetUpInstructions.tsx
@@ -11,14 +11,14 @@ interface ISetUpInstructions {
       alt: string,
     },
     title: string | ReactNode,
-    content: string | ReactNode,
+    content?: string | ReactNode,
     prevLabel?: string,
     nextLabel?: string,
     exitLabel?: string,
-    jump?: {
+    skipTo?: {
       page: number,
       label: string,
-    }
+    },
   }[],
 }
 
@@ -42,33 +42,33 @@ const SetUpInstructions: FC<ISetUpInstructions> = ({
     prevLabel = 'Previous',
     nextLabel = 'Next',
     exitLabel = 'Finish',
-    jump,
+    skipTo,
   } = CurrentPageContent
 
   const prevPage = () => setPage(page - 1)
   const nextPage = () => setPage(page + 1)
-  const jumpPage = () => setPage(jump.page - 1)
+  const jumpPage = () => setPage(skipTo.page - 1)
 
   return (
     <div className='relative w-full h-full'>
-      <div className='flex flex-col content-between w-full h-full justify-center'>
-        { jump && 
+      <div className='flex flex-col w-full h-full'>
+        { skipTo && 
           <div className='flex justify-center p-8 pb-0'>
             <div className='flex justify-end max-w-6xl w-full'>
-              <Button onClick={jumpPage} plain>{jump.label}</Button>
+              <Button onClick={jumpPage} plain>{skipTo.label}</Button>
             </div>
           </div>
         }
         { img &&
-          <div className='flex justify-center w-screen p-8'>
+          <div className='flex justify-center w-full p-8'>
             <div className='max-w-6xl'>
               <img className='w-full' src={img.src} alt={img.alt} />
             </div>
           </div>
         }
-        <div className='flex justify-center p-8'>
+        <div className='flex justify-center p-8 pb-36'>
           <div className='w-full max-w-4xl justify-end'>
-            <div className='flex flex-col space-y-16 overflow-y-auto'>
+            <div className='flex flex-col space-y-16'>
                 { React.isValidElement(title)
                   ? title
                   : <Heading>{title}</Heading>
@@ -84,13 +84,15 @@ const SetUpInstructions: FC<ISetUpInstructions> = ({
             </div>
           </div>
         </div>
-        <div className='flex justify-center mt-auto p-8 border-0 border-t border-solid'>
-          <div className='flex w-full max-w-6xl justify-between gap-2 '>
-            <Button onClick={onFinish} plain>{skipLabel}</Button>
-            <div className='flex gap-4'>
-              { page > 0 && 
-                <Button onClick={prevPage}>{prevLabel}</Button>}
-                <Button onClick={nextPage} primary>{page < pages.length - 1 ? nextLabel: exitLabel}</Button>
+        <div className="fixed bottom-0 w-full bg-neutral-50">
+          <div className='flex justify-center mt-auto p-8 border-0 border-t border-solid'>
+            <div className='flex w-full max-w-6xl justify-between gap-2 '>
+              <Button onClick={onFinish} plain>{skipLabel}</Button>
+              <div className='flex gap-4'>
+                { page > 0 && 
+                  <Button onClick={prevPage}>{prevLabel}</Button>}
+                  <Button onClick={nextPage} primary>{page < pages.length - 1 ? nextLabel: exitLabel}</Button>
+              </div>
             </div>
           </div>
         </div>
