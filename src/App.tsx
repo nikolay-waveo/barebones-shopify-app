92b68377c7da8b1c8834273349bf070cf8c9efc0
nikolay-waveo <nikolay@waveo.com>
2022-01-25 13:37:53 +0800

    Feat: Add PublishSection

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import StorePublishingCard from './components/StorePublishingCard';
 import { usePublisher } from './hooks/usePublisher';
 import { useSubscriber } from './hooks/useSubscriber';
 import ModalWithForm from './components/ModalWithForm';
+import PublishSection from './components/PublishSection';
 
 
 type TLocation = {
@@ -332,205 +333,37 @@ const App: FC<IAppProps> = ({
           fullWidth={true}>
 
           <div className="grid grid-cols-1 gap-10 mb-20">
-            <Section
-              sectionTitle="Publish"
-              sectionDescription="See which stores are subscribed to you." >
+            <PublishSection 
+              user={user}
+              hasError={hasError}
+              errorMessage={errorMessage}
+              publishedTo={publishedTo}
+              isPublishActive={isPublishActive}
+              isPublishPaused={isPublishPaused}
 
-              <StorePublishingCard
-                activated={!isPublishActive}
-                primary={isPublishActive
-                  && {
-                      content: "Link",
-                      onAction: openCalloutCardModal, 
-                      primary: true,   
-                    }}
-                onActivate={{
-                  title: <>Store Publishing <span className="text-red-600">(Disabled)</span></>,
-                  buttonTitle: "Activate",
-                  content: "Allow others to find and subscribe to your store.",
-                  defaultButton: {
-                    content: 'Activate', 
-                    icon: TickMinor,
-                    onAction: handlePublish
-                  }
-                }}
-                onDeactivate={
-                  publishedTo.length > 0
-                  ? {
-                    title: <>Store Publishing <span className={isPublishPaused ? 'text-cyan-600': 'text-green-600'}>
-                      ({isPublishPaused ? 'Paused' : 'Active'})
-                      </span></>,
-                    buttonTitle: "Options",
-                    content: "Share your store with others, pause publishing or disconnect all stores currently subscribed to you.",
-                    sections: [{
-                      title: 'Publish options',
-                      items: [
-                        {
-                          content: isPublishPaused
-                            ? 'Resume' 
-                            : 'Pause',
-                          active: isPublishPaused,
-                          icon: isPublishPaused 
-                            ? PlayMinor
-                            : PauseMinor,
-                          onAction: handlePause
-                        },
-                        {
-                          destructive: true,
-                          content: 'Disconnect All',
-                          icon: CancelSmallMinor,
-                          onAction: openDisconnectAllModal
-                        },
-                      ],
-                    }],
-                  }
-                : {
-                  title: <>Store Publishing <span className="text-green-600">(Active)</span></>,
-                  buttonTitle: "Options",
-                  content: "Share your store with others or disable publishing.",
-                  sections: [{
-                    title: 'Publish options',
-                    items: [
-                      {
-                        destructive: true,
-                        content: 'Disable',
-                        icon: CancelSmallMinor,
-                        onAction: openDeactivatePublishModal
-                      },
-                    ],
-                  }],
-                  }} />
+              handlePause={handlePause}
+              handlePublish={handlePublish}
+              toggleHasError={toggleHasError}
+              onPublishConnect={onPublishConnect}
+              onPublishDisconnect={onPublishConnect}
 
-              { isPublishActive && 
-                <List
-                  list={publishedTo}
-                  listText={{
-                    title: isPublishPaused 
-                      ? <>Subscribers <span className="text-cyan-600">(Paused)</span></>
-                      : "Subscribers",
-                    description: "You can connect, disconnect and track subscriptions to your store.",
-                  }}
-                  emptyListText={{
-                    title: "No subscribers yet",
-                    description: "Track user subscriptions to your store."
-                  }} 
-                  renderItem={(item) => { 
-                    return (
-                      <ResourceItem
-                        id={item.id}
-                        onClick={() => {}}>
-                        <Item
-                          item={item} 
-                          loading={{
-                            accessibilityLabel: "Sending request",
-                          }}
-                          badges={[
-                            {
-                              status: "pending",
-                              tooltip: "Waiting for publisher confirmation",
-                              statusStyle: "new",
-                            },
-                            {
-                              status: "active",
-                              displayStatus: isPublishPaused ? "paused" : "active",
-                              tooltip: isPublishPaused ? "Publishing is currently paused" : "There is an active subscription",
-                              statusStyle: isPublishPaused ? "info" : "success",
-                            },
-                            {
-                              status: "stopped",
-                              tooltip: "The publisher has stopped the connection",
-                              statusStyle: "critical",
-                            },
-                            {
-                              status: "declined",
-                              tooltip: "The publisher has declined your subscription request",
-                              statusStyle: "warning",
-                            }
-                          ]}
-                          options={[
-                            item.status !== 'active' 
-                            ? {
-                              content: 'Connect',
-                              helpText: "Accept subscription to your store",
-                              icon: TickMinor,
-                              onAction: () => onPublishConnect(item),
-                              active: true,
-                            }
-                            : null,
-                            {
-                              content: 'Disconnect',
-                              helpText: "Deny subscription to your store",
-                              icon: CancelSmallMinor,
-                              onAction: () => onPublishDisconnect(item),
-                              destructive: true,
-                            },
-                          ]} />
-                        </ResourceItem> 
-                      )
-                  }}/> }
+              showCalloutCardModal={showCalloutCardModal}
+              openCalloutCardModal={openCalloutCardModal}
+              closeCalloutCardModal={closeCalloutCardModal}
+              setShowCalloutCardModal={setShowCalloutCardModal}
 
-              {hasError && <Toast content={errorMessage} error onDismiss={toggleHasError} /> }
+              showDisconnectAllModal={showDisconnectAllModal}
+              openDisconnectAllModal={openDisconnectAllModal}
+              closeDisconnectAllModal={closeDisconnectAllModal}
+              handleDisconnectAllModal={handleDisconnectAllModal}
+              setShowDisconnectAllModal={setShowDisconnectAllModal}
 
-              <Modal
-                title="Disconnect All Subscriptions and Disable Publishing"
-                content="All current subscriptions to your store will be disconnected 
-                  and publishing will be disabled. Do you wish to continue?" 
-                isModalOpen={showDisconnectAllModal}
-                modalHandler={setShowDisconnectAllModal} 
-                primaryAction={{
-                  actionText: "Disconnect All",
-                  actionHandler: handleDisconnectAllModal,
-                  destructive: true
-                }}
-                secondaryActions={[
-                  {
-                    actionText: "Cancel",
-                    actionHandler: closeDisconnectAllModal,
-                  },
-                ]}
-                toast={{
-                  content: "Subscriptions Disconnected"
-                }} />
-
-              <Modal
-                title="Disable Publishing"
-                content="Disabling this setting will stop others from finding your 
-                  store. Do you wish to continue?" 
-                isModalOpen={showDeactivatePublishModal}
-                modalHandler={setShowDeactivatePublishModal} 
-                primaryAction={{
-                  actionText: "Disable",
-                  actionHandler: handleDeactivatePublishModal,
-                  destructive: true
-                }}
-                secondaryActions={[
-                  {
-                    actionText: "Cancel",
-                    actionHandler: closeDeactivatePublishModal,
-                  },
-                ]}
-                toast={{
-                  content: "Publishing Disabled"
-                }} />
-
-              <Modal
-                title="Get your store link"
-                content={
-                  <p>
-                    Your store link is <TextStyle variation="code">{user}</TextStyle>. 
-                    Share it with others so that they can find and subscribe to your store.
-                  </p>
-                }
-                isModalOpen={showCalloutCardModal}
-                modalHandler={setShowCalloutCardModal} 
-                primaryAction={{
-                  actionText: "Continue",
-                  actionHandler: (e) => {
-                    setShowCalloutCardModal(false)
-                  },
-                }} />
-              
-            </Section>
+              showDeactivatePublishModal={showDeactivatePublishModal}
+              openDeactivatePublishModal={openDeactivatePublishModal}
+              closeDeactivatePublishModal={closeDeactivatePublishModal}
+              handleDeactivatePublishModal={handleDisconnectAllModal}
+              setShowDeactivatePublishModal={setShowDeactivatePublishModal}
+              />
 
             <Section
               sectionTitle="Subscribe"
