a3844a6f24dec60103504e6fac16d3e0454b9ad7
nikolay-waveo <nikolay@waveo.com>
2022-01-27 19:38:48 +0800

    Feat: Create publishing page

---
+++ src/components/PublishingPage.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@shopify/polaris';
+import { FC, useState } from 'react';
+
+type TAction = {
+  actionHandler: (e?: any) => void,
+}
+
+interface IPublishingPage {
+  action: TAction
+}
+
+const PublishingPage: FC<IPublishingPage> = ({
+  action
+}) => {
+  const [ publishing, setPublishing ] = useState(false)
+
+  const {
+    actionHandler
+  } = action
+
+  return (
+    <div className='flex flex-col space-y-10 text-emerald-800'>
+      <p className='font-medium text-center'>
+        Allow others to find and subscribe to your store by enabling <span className='text-amber-600'>Publishing</span>.
+      </p>
+      <div>
+        <Button 
+          fullWidth 
+          primary={!publishing} 
+          onClick={() => {
+            actionHandler(!publishing)
+            setPublishing(!publishing)
+          }} >
+            { !publishing
+              ? 'Enable Publishing'
+              : 'Disable Publishing'
+            } 
+        </Button>
+        <p className='text-center mt-4'>
+          You can always disable this later in the publish section.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PublishingPage;
